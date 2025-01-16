f1df43c66ac838d8421baf0e2cac71ac0f270be2
Andrew Virts <asvirts@outlook.com>
2025-01-16 09:46:12 -0600

    Enhance Tailwind configuration and update homepage layout: Added custom color palette and font families in Tailwind config. Improved homepage structure with new layout styles, integrated logo image, and added a call-to-action link for booking a consultation.

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import { Instrument_Serif } from "next/font/google"
-
+import Link from "next/link"
+import Image from "next/image"
 const instrumentSerif = Instrument_Serif({
   subsets: ["latin"],
   display: "swap",
@@ -9,13 +10,25 @@ const instrumentSerif = Instrument_Serif({
 
 export default function Home() {
   return (
-    <div className="">
+    <div className="flex flex-col items-center justify-center py-16 gap-8">
+      <Image
+        src="/virts-logo.svg"
+        alt="Virts Creative Logo"
+        width={36}
+        height={36}
+      />
       <h1
-        className={`text-6xl font-bold text-center ${instrumentSerif.className}`}
+        className={`text-6xl mx-auto max-w-5xl font-bold text-center ${instrumentSerif.className}`}
       >
         I elevate startups and small-medium businesses through design and
         development
       </h1>
+      <Link
+        href="/contact"
+        className="mt-8 mx-auto px-8 py-4 bg-yellow-500 text-text-dark rounded-full hover:opacity-90 transition-opacity"
+      >
+        Book my 15-minute intro call
+      </Link>
     </div>
   )
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,36 @@
-import type { Config } from "tailwindcss";
+import type { Config } from "tailwindcss"
 
 export default {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}"
   ],
   theme: {
     extend: {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        yellow: {
+          50: "#fef9ed",
+          100: "#fdecc6",
+          200: "#fbe3aa",
+          300: "#fad684",
+          400: "#f9ce6c",
+          500: "#f7c247",
+          600: "#e1b141",
+          700: "#af8a32",
+          800: "#886b27",
+          900: "#68511e"
+        },
+        dark: "#242424",
+        light: "#8E8E8E"
       },
-    },
+      fontFamily: {
+        helvetica: ["Helvetica", "sans-serif"],
+        instrumentSerif: ["Instrument Serif", "serif"]
+      }
+    }
   },
-  plugins: [],
-} satisfies Config;
+  plugins: []
+} satisfies Config
