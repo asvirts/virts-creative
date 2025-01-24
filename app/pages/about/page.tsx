a99a28b3df4d4f9debc93f829b10704f2ae5ec84
Andrew Virts <asvirts@outlook.com>
2025-01-24 13:59:05 -0600

    updates

--- app/pages/about/page.tsx
+++ app/pages/about/page.tsx
@@ -1,5 +1,6 @@
 import { Instrument_Serif } from "next/font/google"
 import Image from "next/image"
+
 const instrumentSerif = Instrument_Serif({
   subsets: ["latin"],
   display: "swap",
@@ -44,7 +45,7 @@ export default function About() {
         <p className="text-sm">“Andrew was so great to work with!”</p>
       </div>
       <Image
-        src="/virts-logo.svg" 
+        src="/virts-logo.svg"
         alt="Virts Creative Logo"
         width={36}
         height={36}
@@ -60,6 +61,34 @@ export default function About() {
         experiences that marry beauty with purpose, ensuring your users
         don&apos;t just see the design—they feel it working for them.
       </p>
+      <section>
+        <section id="relume" className="px-[5%] py-16 md:py-24 lg:py-28">
+          <div className="container">
+            <div className="grid grid-cols-1 gap-y-12 md:grid-cols-2 md:items-center md:gap-x-12 lg:gap-x-20">
+              <div>
+                <h1 className="rb-5 mb-5 text-4xl font-bold md:mb-6 md:text-5xl lg:text-6xl ${helvetica.className} text-dark">
+                  Long heading is what you see here in this feature section
+                </h1>
+                <p className="md:text-md text-light">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Suspendisse varius enim in eros elementum tristique. Duis
+                  cursus, mi quis viverra ornare, eros dolor interdum nulla, ut
+                  commodo diam libero vitae erat.
+                </p>
+              </div>
+              <div>
+                <Image
+                  src="https://67wvo3jvf7.ufs.sh/f/31uuUYnOr3SZmjeJPfBJFz4nbW2LMDNtOP7jpI1sgh5ScClu"
+                  className="w-full object-cover"
+                  alt="Relume placeholder image"
+                  width={500}
+                  height={500}
+                />
+              </div>
+            </div>
+          </div>
+        </section>
+      </section>
     </div>
   )
 }
